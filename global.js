57a4cc8bb10945c1d516fc2e9cb80d75f1c9a3be
ChidiebereAgunanne <cagunanne@ucsd.edu>
2026-05-06 13:48:55 -0700

    lab 6 up to step 2

--- global.js
+++ global.js
@@ -3,6 +3,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
   { url: 'resume/', title: 'Resume' },
+  { url: 'meta/', title: 'Meta' },
   { url: 'https://github.com/ChidiebereAgunanne', title: 'GitHub' }
 ];
 const BASE_PATH =
